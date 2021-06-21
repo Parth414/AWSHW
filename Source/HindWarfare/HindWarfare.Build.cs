6dcaf92176349d0bd9abab9af70f1f61d5df5105
Mujahid Shaikh <mbshaikh19@gmail.com>
2021-06-21 16:39:15 +0530

    HindWarfare project migration
    Added player attacker
    Added CPP character base
    Removed Game_Character
    Added physics surface types

--- Source/HindWarfare/HindWarfare.Build.cs
+++ Source/HindWarfare/HindWarfare.Build.cs
@@ -6,6 +6,6 @@ public class HindWarfare : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PhysicsCore" });
 	}
 }
